4d3b4b87d8e7f7a58cd97e8770553551a428ce99
mengqingmeng <762577099@qq.com>
2017-07-25 09:18:12 +0800

    startTF

---
+++ gpus.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+#查看计算设备信息，是否是gpu计算
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
+
+print("---------------------手动使用cpu------------------")
+with tf.device('/cpu:0'):
+  a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+  b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+
+#但是在计算matmul时，又切换到gpu模式
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
--- startTF.py
+++ startTF.py
@@ -1,4 +1,7 @@
+#conding=utf-8
 import tensorflow as tf
+import numpy as np
+
 #定义常量
 node1 = tf.constant(3.0, dtype=tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
@@ -32,21 +35,92 @@ print(sess.run(add_and_triple, {a: 3, b:4.5}))
 #定义变量
 W = tf.Variable([.3], dtype=tf.float32)
 b = tf.Variable([-.3], dtype=tf.float32)
-x = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32) #输入
 linear_model = W * x + b
 #使用变量，必须使用特定的语句初始化，在运行sess.run 之前，变量是未初始化的
 init = tf.global_variables_initializer()
 sess.run(init)
 print(sess.run(linear_model, {x:[1,2,3,4]}))
 
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+
 #计算损失值
 y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))#23.66
+print(sess.run(loss, {x:x_train, y:y_train}))#23.66
 
 #损失值较大，所以手动更新权重和偏差，尝试计算损失值
 fixW = tf.assign(W, [-1.])
 fixb = tf.assign(b, [1.])
 sess.run([fixW, fixb])
-print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+print(sess.run(loss, {x:x_train, y:y_train}))
+
+
+# optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# training loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init) # reset values to wrong
+for i in range(1000):
+  sess.run(train, {x:x_train, y:y_train})
+
+# evaluate training accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
+
+print('-------------------tf.contrib.learn 实现线性回归------------------------')
+features = [tf.contrib.layers.real_valued_column("x", dimension=1)]
+
+estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
+x_train = np.array([1., 2., 3., 4.])
+y_train = np.array([0., -1., -2., -3.])
+x_eval = np.array([2., 5., 8., 1.])
+y_eval = np.array([-1.01, -4.1, -7, 0.])
+
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x_train}, y_train,
+                                              batch_size=4,
+                                              num_epochs=1000)
+eval_input_fn = tf.contrib.learn.io.numpy_input_fn(
+    {"x":x_eval}, y_eval, batch_size=4, num_epochs=1000)
+
+#训练
+estimator.fit(input_fn=input_fn, steps=1000)
+
+train_loss = estimator.evaluate(input_fn=input_fn)
+eval_loss = estimator.evaluate(input_fn=eval_input_fn)
+print("train loss: %r"% train_loss)
+print("eval loss: %r"% eval_loss)
+
+print('-------------------自定义模型------------------------')
+# Declare list of features, we only have one real-valued feature
+def model(features, labels, mode):
+  # Build a linear model and predict values
+  W = tf.get_variable("W", [1], dtype=tf.float64)
+  b = tf.get_variable("b", [1], dtype=tf.float64)
+  y = W*features['x'] + b
+  # Loss sub-graph
+  loss = tf.reduce_sum(tf.square(y - labels))
+  # Training sub-graph
+  global_step = tf.train.get_global_step()
+  optimizer = tf.train.GradientDescentOptimizer(0.01)
+  train = tf.group(optimizer.minimize(loss),
+                   tf.assign_add(global_step, 1))
+  # ModelFnOps connects subgraphs we built to the
+  # appropriate functionality.
+  return tf.contrib.learn.ModelFnOps(
+      mode=mode, predictions=y,
+      loss=loss,
+      train_op=train)
+
+estimator = tf.contrib.learn.Estimator(model_fn=model)
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x": x_train}, y_train, 4, num_epochs=1000)
+estimator.fit(input_fn=input_fn, steps=1000)
+train_loss = estimator.evaluate(input_fn=input_fn)
+eval_loss = estimator.evaluate(input_fn=eval_input_fn)
+print("train loss: %r"% train_loss)
+print("eval loss: %r"% eval_loss)
